7ce4f45607dbbabab59a37b2d09cd2a3869d1952
dinesh <dineshpy07@gmail.com>
2015-10-11 01:50:23 +0530

    Remove readme dependency

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
-import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "look-up",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Dinesh S",
     author_email = "dineshpy07@gmail.com",
     description = ("Get the meaning of a word on your terminal"),
@@ -18,7 +11,8 @@ setup(
     keywords = "english dictionary words",
     url = "https://github.com/Dineshs91/lookup",
     packages = ['lookup'],
-    long_description = read('README.md'),
+    long_description = "Get the meaning of a word, its pronunciation and "\
+                       "examples. Also get word of the day",
     install_requires = [
         "click>=5.0",
         "wordnik==2.1.3"
